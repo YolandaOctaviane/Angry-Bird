bc64ce305c575decba760f6b98d055463c24ac42
yolanda octaviane <yolandaoctavi@gmail.com>
2021-09-22 19:28:23 +0700

    Initial commit

---
+++ Assets/Scripts/Bird.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Bird : MonoBehaviour
+{
+
+    public UnityAction OnBirdDestroyed = delegate { };
+    public UnityAction<Bird> OnBirdShot = delegate { };
+    public enum BirdState { Idle, Thrown, HitSomething }
+    public Rigidbody2D RigidBody;
+    public CircleCollider2D Collider;
+    public BirdState State;
+
+    private float _minVelocity = 0.05f;
+    private bool _flagDestroy = false;
+
+    void Start()
+    {
+        RigidBody.bodyType = RigidbodyType2D.Kinematic;
+        Collider.enabled = false;
+        State = BirdState.Idle;
+    }
+
+    void FixedUpdate()
+    {
+        if (State == BirdState.Idle &&
+            RigidBody.velocity.sqrMagnitude >= _minVelocity)
+        {
+            State = BirdState.Thrown;
+        }
+
+        if ( (State == BirdState.Thrown || State == BirdState.HitSomething )&&
+            RigidBody.velocity.sqrMagnitude < _minVelocity &&
+            !_flagDestroy)
+        {
+            //Hancurkan gameobject setelah 2 detik
+            //jika kecepatannya sudah kurang dari batas minimum
+            _flagDestroy = true;
+            StartCoroutine(DestroyAfter(2));
+        }
+
+    }
+
+    private IEnumerator DestroyAfter(float second)
+    {
+        yield return new WaitForSeconds(second);
+        Destroy(gameObject);
+    }
+
+    public void MoveTo(Vector2 target, GameObject parent)
+    {
+        gameObject.transform.SetParent(parent.transform);
+        gameObject.transform.position = target;
+    }
+
+    public void Shoot(Vector2 velocity, float distance, float speed)
+    {
+        Collider.enabled = true;
+        RigidBody.bodyType = RigidbodyType2D.Dynamic;
+        RigidBody.velocity = distance * speed * velocity;
+        OnBirdShot(this);
+    }
+
+    void OnDestroy()
+    {
+        if (State == BirdState.Thrown || State == BirdState.HitSomething)
+        {
+            OnBirdDestroyed();
+        }
+    }
+    public virtual void OnCollisionEnter2D(Collision2D col)
+    {
+        State = BirdState.HitSomething;
+        Debug.Log("Hit something Debug");
+    }
+
+    
+    public virtual void OnTap()
+    {
+        //Do nothing
+    }
+}
---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,111 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameController : MonoBehaviour
+{
+    public SlingShooter SlingShooter;
+    public TrailController TrailController;
+    public List<Bird> Birds;
+    public List<Enemy> Enemies;
+    public Button nextButton;
+    public GameObject GameOverPanel;
+
+    private Bird _shotBird;
+
+    public BoxCollider2D TapCollider;
+
+    private bool _isGameEnded = false;
+    void Start()
+    {
+        nextButton.interactable = false;
+        for (int i = 0; i < Birds.Count; i++)
+        {
+            //object game controller akan meng "subscribe" fungsi onbirddestroyed , dan menjalankan fungsi Change bird
+            Birds[i].OnBirdDestroyed += ChangeBird;
+
+            Birds[i].OnBirdShot += AssignTrail;
+        }
+
+        for (int i = 0; i < Enemies.Count; i++)
+        {
+            Enemies[i].OnEnemyDestroyed += CheckGameEnd;
+        }
+        TapCollider.enabled = false;
+        SlingShooter.InitiateBird(Birds[0]);
+        _shotBird = Birds[0];
+    }
+
+    public void ChangeBird()
+    {
+        _shotBird = null;
+
+        TapCollider.enabled = false;
+
+        if (_isGameEnded)
+        {
+            return;
+        }
+
+        Birds.RemoveAt(0);
+
+        if (Birds.Count > 0)
+        {
+            SlingShooter.InitiateBird(Birds[0]);
+            _shotBird = Birds[0];
+            return;
+        }
+        if (Enemies.Count > 0 && Birds.Count <= 0 && !_isGameEnded)
+        {
+            GameOverPanel.SetActive(true);
+
+            _isGameEnded = true;
+            return;
+        }
+    }
+
+    public void CheckGameEnd(GameObject destroyedEnemy)
+    {
+        for (int i = 0; i < Enemies.Count; i++)
+        {
+            if (Enemies[i].gameObject == destroyedEnemy)
+            {
+                Enemies.RemoveAt(i);
+                break;
+            }
+        }
+        if(Enemies.Count > 0 && Birds.Count <= 0)
+        {
+            GameOverPanel.SetActive(true);
+
+            _isGameEnded = true;
+            return;
+        }
+        if (Enemies.Count == 0)
+        {
+            _isGameEnded = true;
+            nextButton.interactable = true;
+        }
+    }
+    public void AssignTrail(Bird bird)
+    {
+        TrailController.SetBird(bird);
+        StartCoroutine(TrailController.SpawnTrail());
+        TapCollider.enabled = true;
+    }
+    void OnMouseUp()
+    {
+        if (_shotBird != null)
+        {
+            _shotBird.OnTap();
+        }
+    }
+
+    public void LoadNextLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex+1);
+    }
+
+}
